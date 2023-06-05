0bb0b3b89aab6352af161587fcab0ba9640a68a9
kdj <es-kbsys24@hybecorp.com>
2023-06-05 17:11:30 +0900

    emtion styled, global.css setting

---
+++ src/components/blog/blogHeader/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Styled from "./styled";
+
+function blogHeader() {
+  return <Styled></Styled>;
+}
+
+export default blogHeader;
---
+++ src/pages/blogPage/styled.js
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+const DivStyled = styled.div``;
+
+export default DivStyled;
