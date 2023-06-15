7309fee17fb64fdae36b23405a01ebe874d680e4
kdj <es-kbsys24@hybecorp.com>
2023-06-16 01:09:17 +0900

    redux toolkit 완료

--- src/store/shopBasket/shopBasket.js
+++ src/store/shopBasket/shopBasket.js
@@ -6,11 +6,23 @@ let shopBasket = createSlice({
   initialState: data,
   reducers: {
     countUp(state, i) {
-      state[i.payload].count += 1;
+      let index = state.findIndex((obj) => obj.id === i.payload);
+      state[index].count++;
+    },
+    cartAdd(state, add) {
+      let obj = {
+        id: add.payload.id,
+        name: add.payload.title,
+        count: 1,
+      };
+      state.push(obj);
+
+      //장바구니 삭제기능 추가고민
+      //장바구니 중복값 추가하면 수치 증가
     },
   },
 });
 
-export let { countUp } = shopBasket.actions;
+export let { countUp, cartAdd } = shopBasket.actions;
 
 export default shopBasket;
