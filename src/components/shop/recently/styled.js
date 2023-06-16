b0dacb1f59830a8c9882c16dcc4bf75fa9173304
kdj <es-kbsys24@hybecorp.com>
2023-06-17 02:38:12 +0900

    local storage

---
+++ src/components/shop/recently/styled.js
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+const Styled = styled.div`
+  position: fixed;
+  width: 120px;
+  min-height: 200px;
+  padding: 10px;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 3px;
+  right: 10px;
+  bottom: 10px;
+  z-index: 10;
+
+  .recently-title {
+    text-align: center;
+  }
+`;
+
+export default Styled;
